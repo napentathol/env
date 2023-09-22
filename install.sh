d1bb6533f2118ae83ea40d7809ce67cbf2e5c787
napentathol <napentathol@users.noreply.github.com>
2023-09-22 14:25:49 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,8 @@
 cd ~
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone https://github.com/napentathol/env.git
-rm ~/.zshrc ; ln -s ~/.env/.zshrc ~/.zshrc
-rm ~/.vimrc ; ln -s ~/.env/.vimrc
-rm ~/.gitconfig ; ln -s ~/.env/.gitconfig
+rm ~/.zshrc ; ln -s ~/env/.zshrc ~/.zshrc
+rm ~/.vimrc ; ln -s ~/env/.vimrc
+rm ~/.gitconfig ; ln -s ~/env/.gitconfig
 ln -s ~/env/acw.zsh-theme ~/.oh-my-zsh/custom/themes/acw.zsh-theme
+touch env/.env
